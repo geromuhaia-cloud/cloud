8f54237bfc02bd021f17be29868d83ba5c88f913
geromuhaia-cloud <geronimuhaia2@gmail.com>
2024-08-02 18:46:58 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('.menu').click(function() {
+        $('.menu').toggleClass('active')
+        $('.nav-bar').toggleClass('open')
+        $('.flex').toggleClass('effect')
+    })
+})
+
+$(document).ready(function(){
+    $('.item1').click(function(){
+        $('.nav1').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('.item2').click(function(){
+        $('.nav2').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('.item3').click(function(){
+        $('.nav3').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('.item4').click(function(){
+        $('.nav4').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('.item5').click(function(){
+        $('.nav5').toggleClass('show')
+    })
+})
+
+$(document).ready(function(){
+    $('.item6').click(function(){
+        $('.nav6').toggleClass('show')
+    })
+})
